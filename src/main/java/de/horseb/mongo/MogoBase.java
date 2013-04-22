0f75121a59d39a10e4a126d7b87b37eba09ae63f
Sebastian <s.zechlin@gmail.com>
2013-04-22 23:29:09 +0200

    Gebaut als war, erste Verbindung zur DB

--- src/main/java/de/horseb/mongo/MogoBase.java
+++ src/main/java/de/horseb/mongo/MogoBase.java
@@ -10,24 +10,28 @@ import com.mongodb.MongoClient;
 
 public class MogoBase {
 
-	public void insertOrUpdateDocumentInCollection(BasicDBObject document,final String collectionName) throws UnknownHostException{
+	public void insertOrUpdateDocumentInCollection(BasicDBObject document,
+			final String collectionName) throws UnknownHostException {
 		getCollection(collectionName).save(document);
 	}
-	
-	public DBCursor getAllEntriesInCollection(final String collectionName) throws UnknownHostException{
+
+	public DBCursor getAllEntriesInCollection(final String collectionName)
+			throws UnknownHostException {
 		DBCursor result = getCollection(collectionName).find();
 		return result;
 	}
-	
-	private DBCollection getCollection(final String collectionName) throws UnknownHostException{
+
+	private DBCollection getCollection(final String collectionName)
+			throws UnknownHostException {
 		DBCollection result = getConnection().getCollection(collectionName);
 		return result;
 	}
-	
-	private DB getConnection() throws UnknownHostException{
-		MongoClient client = new MongoClient( "localhost" , 28017 );
-		DB db = client.getDB("database name");
+
+	private DB getConnection() throws UnknownHostException {
+		MongoClient client = new MongoClient("localhost", 27017);
+		DB db = client.getDB("test");
 		// boolean auth = db.authenticate("username", "password".toCharArray());
-		return db;	
+		return db;
 	}
+
 }
--- src/main/java/de/horseb/services/AnleitungService.java
+++ src/main/java/de/horseb/services/AnleitungService.java
@@ -1,21 +1,49 @@
 package de.horseb.services;
 
+import java.net.UnknownHostException;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import de.horseb.mongo.MogoBase;
 
 @Path("/anleitung")
 public class AnleitungService {
 
 	@GET
-	@Path("/{id}")
-	public Response printMessage(@PathParam("param") String id) {
- 
-		String result = "Restful example : " + id;
- 
-		return Response.status(200).entity(result).build();
- 
+	@Path("/getAll")
+	@Produces("application/json")
+	public List<DBObject> getAnleitungen() {
+		MogoBase mongo = new MogoBase();
+		DBCursor result = null;
+		try {
+			result = mongo.getAllEntriesInCollection("things");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		StringBuffer sb = new StringBuffer();
+		// while (result.hasNext()) {
+		// sb.append(result.toString());
+		// }
+		return result.toArray();
 	}
-	
+
+	@GET
+	@Path("/{id}")
+	public Response getAnleitung(@PathParam("param") String id) {
+
+		String result = "Restful example : " + id;
+
+		return Response.status(200).entity(result).build();
+
+	}
+
 }
