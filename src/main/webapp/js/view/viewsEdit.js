599bee5671e1a92078eac02c0641f9417c165046
seze <s.zechlin@gmail.com>
2013-07-22 15:39:48 +0200

    #8 wysihtml5-bootstrap eingebunden

--- src/main/webapp/js/view/viewsEdit.js
+++ src/main/webapp/js/view/viewsEdit.js
@@ -1,25 +1,3 @@
-var TitelEditView = Backbone.View.extend({
-
-    tagName: "div",
-
-    templateEdit: _.template($('#template-TitelEdit').html()),
-
-    read: function () {
-        var h = this.$el.find('#haupttitel').val();
-        var u = this.$el.find('#untertitel').val();
-        this.model.set({
-            haupttitel: h,
-            untertitel: u
-        });
-    },
-
-    render: function () {
-        this.$el.html(this.templateEdit(this.model.toJSON()));
-        return this;
-    }
-
-});
-
 var SchrittEditView = Backbone.View.extend({
 
     tagName: "div",
@@ -52,10 +30,18 @@ var SchrittEditView = Backbone.View.extend({
     },
 
     createTextarea: function () {
-        var editor = new wysihtml5.Editor("schritt-textarea", {
-            toolbar: "wysihtml5-toolbar",
-            parserRules: wysihtml5ParserRules
+
+        $('#schritt-textarea').wysihtml5({
+            "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+            "emphasis": true, //Italics, bold, etc. Default true
+            "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+            "html": false, //Button which allows you to edit the generated HTML. Default false
+            "link": true, //Button to insert a link. Default true
+            "image": true, //Button to insert an image. Default true,
+            "color": false //Button to change color of font
         });
+
+
     }
 
 });
@@ -75,7 +61,7 @@ var AnleitungEditView = Backbone.View.extend({
 
     events: {
         'click #addSchritt': 'addSchritt',
-        'click #speichern': 'saveAnleitung',
+        'click #speichern': 'saveAnleitung'
 
     },
 
@@ -86,18 +72,26 @@ var AnleitungEditView = Backbone.View.extend({
             haupttitel: h,
             untertitel: u
         });
+       this.readSchritte() ;
+        this.model.save();
+    },
+
+    readSchritte: function(){
         if (this.schritteEditView.length) {
             console.log("Es gibt Schritte zu speichern: "
                 + this.schritteEditView.length);
             _.each(this.schritteEditView, function (schrittEditView) {
                 console.log("read Schritt");
+                if(schrittEditView instanceof SchrittEditView) {
                 schrittEditView.read();
+                }
             });
         }
-        this.model.save();
     },
 
     addSchritt: function () {
+        this.readSchritte();
+        this.render();
         var schritt = new Schritt();
         this.schritte.add(schritt);
         var view = new SchrittEditView({
@@ -119,7 +113,7 @@ var AnleitungEditView = Backbone.View.extend({
     renderSchritte: function () {
         if (this.schritte.length) {
             this.schritte.each(function (schritt) {
-                var view = new SchrittEditView({
+                var view = new SchrittView({
                     model: schritt
                 });
                 this.$el.append(view.render().el);
