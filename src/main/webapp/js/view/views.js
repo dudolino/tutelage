4a3eaf44a8ee3544ed4dbd91a2a80be69b7cb924
Sebastian <s.zechlin@gmail.com>
2013-08-01 23:15:36 +0200

    SchrittView angehübscht

--- src/main/webapp/js/view/views.js
+++ src/main/webapp/js/view/views.js
@@ -1,131 +1,126 @@
 var SchrittView = Backbone.View.extend({
 
-    tagName: "table",
+	template : _.template($('#template-Schritt').html()),
 
-    className: "hero-unit",
-
-    template: _.template($('#template-Schritt').html()),
-
-    render: function () {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
-    }
+	render : function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
 });
 
 var SchritteView = Backbone.View.extend({
 
-    render: function () {
-        if (this.model.length) {
-            this.model.each(function (schritt) {
-                this.$el.append(new SchrittView({
-                    model: schritt
-                }).render().el);
-            }, this);
-            return this;
-        }
-    }
+	render : function() {
+		if (this.model.length) {
+			this.model.each(function(schritt) {
+				this.$el.append(new SchrittView({
+					model : schritt
+				}).render().el);
+			}, this);
+			return this;
+		}
+	}
 });
 
 var MaterialView = Backbone.View.extend({
 
-    tagName: "tr",
+	tagName : "tr",
 
-    template: _.template($('#template-Material').html()),
+	template : _.template($('#template-Material').html()),
 
-    render: function () {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
-    }
+	render : function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
 });
 
 var MaterialListeView = Backbone.View.extend({
 
-    tagName: "table",
+	tagName : "table",
 
-    className: "table",
+	className : "table",
 
-    render: function () {
-        if (this.model.length) {
-            this.model.each(function (material) {
-                this.$el.append(new MaterialView({
-                    model: material
-                }).render().el);
-            }, this);
-            return this;
-        }
-    }
+	render : function() {
+		if (this.model.length) {
+			this.model.each(function(material) {
+				this.$el.append(new MaterialView({
+					model : material
+				}).render().el);
+			}, this);
+			return this;
+		}
+	}
 });
 
 var AnleitungView = Backbone.View.extend({
 
-    template: _.template($('#template-TitelHeader').html()),
+	template : _.template($('#template-TitelHeader').html()),
 
+	render : function() {
+		this.$el.append(this.template(this.model.toJSON()));
+		this.renderSchritte();
+		this.renderMaterial();
+		return this;
 
-    render: function () {
-        this.$el.append(this.template(this.model.toJSON()));
-        this.renderSchritte();
-        this.renderMaterial();
-        return this;
+	},
 
-    },
+	renderSchritte : function() {
+		var schritte = this.model.get('schritte');
+		if (schritte.length) {
+			schritte.each(function(schritt) {
+				this.$el.append(new SchrittView({
+					model : schritt
+				}).render().el);
+			}, this);
+		}
+	},
 
-    renderSchritte: function () {
-        var schritte = this.model.get('schritte');
-        if (schritte.length) {
-            schritte.each(function (schritt) {
-                this.$el.append(new SchrittView({
-                    model: schritt
-                }).render().el);
-            }, this);
-        }
-    },
+	renderMaterial : function() {
+		var material = this.model.get('material');
+		if (material.length) {
+			material.each(function(material) {
+				this.$el.append(new MaterialView({
+					model : material
+				}).render().el);
+			}, this);
+		}
 
-    renderMaterial: function () {
-        var material = this.model.get('material');
-        if (material.length) {
-            material.each(function (material) {
-                this.$el.append(new MaterialView({
-                    model: material
-                }).render().el);
-            }, this);
-        }
-
-    }
+	}
 
 });
 
 var AnleitungListViewItem = Backbone.View.extend({
 
-    tagName: "div",
+	tagName : "div",
 
-    className: "well well-small",
+	className : "well well-small",
 
-    templateList: _.template($('#template-AnleitungList').html()),
+	templateList : _.template($('#template-AnleitungList').html()),
 
-    render: function () {
-        this.$el.html(this.templateList(this.model.toJSON()));
-        return this;
-    }
+	render : function() {
+		this.$el.html(this.templateList(this.model.toJSON()));
+		return this;
+	}
 });
 
 var AnleitungListView = Backbone.View.extend({
 
-    initialize: function () {
-        this.model.bind('change', this.render, this);
-    },
+	initialize : function() {
+		this.model.bind('change', this.render, this);
+	},
 
-    render: function () {
-        this.$el.empty();
-        if (this.model.length) {
-            new NavigationView({
-                el: $('#main')
-            });
-            this.model.each(function (anleitung) {
-                this.$el.append(new AnleitungListViewItem({
-                    model: anleitung
-                }).render().el);
-            }, this);
-        }
-        return this;
-    }
+	render : function() {
+		this.$el.empty();
+		if (this.model.length) {
+			new NavigationView({
+				el : $('#main')
+			});
+			this.model.each(function(anleitung) {
+				this.$el.append(new AnleitungListViewItem({
+					model : anleitung
+				}).render().el);
+			}, this);
+		}
+		return this;
+	}
 });
